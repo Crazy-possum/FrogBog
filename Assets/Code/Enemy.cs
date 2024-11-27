bbc58ea4dd9af424db92d80dbbf9ca63dfce1fd6
Crazy-possum <marlizon04@yandex.ru>
2024-11-27 17:35:45 +0300

    EnableEnemySprite

---
+++ Assets/Code/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _enemySprite;
+
+    private void Start()
+    {
+        StageController.Instance.OnChangeStage += ChangeEnemyState;
+    }
+
+    private void OnDestroy()
+    {
+        StageController.Instance.OnChangeStage -= ChangeEnemyState;
+    }
+
+    private void ChangeEnemyState(int stageInt)
+    {
+        if (stageInt == 2)
+        {
+            _enemySprite.enabled = false;
+        }
+        else if (stageInt == 3)
+        {
+            _enemySprite.enabled = true;
+        }
+    }
+}
+
--- Assets/Code/StageController.cs
+++ Assets/Code/StageController.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 
 public class StageController : MonoBehaviour
 {
+    private static StageController _instance;
     [SerializeField] private CameraTrigger _cameraTrigger;
     [SerializeField] private PlayerTrigger _playerTrigger;
     [SerializeField] private CameraMovement _cameraMovement;
     [SerializeField] private GameObject _camera;
     [SerializeField] private GameObject _player;
+    private Action<int> _onChangeStage;
+
+    public static StageController Instance { get => _instance; set => _instance = value; }
+    public Action<int> OnChangeStage { get => _onChangeStage; set => _onChangeStage = value; }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if(_instance == this)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
@@ -25,15 +42,16 @@ public class StageController : MonoBehaviour
 
     private void ChangeToSecondStage()
     {
+        OnChangeStage?.Invoke(2);
         _camera.SetActive(false);
         _player.SetActive(true);
     }
 
     private void ChangeToThirdStage()
     {
+        OnChangeStage?.Invoke(3);
         _cameraMovement.ToStartPosition();
         _player.SetActive(false);
         _camera.SetActive(true);
     }
 }
-
